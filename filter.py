5886c39606a0ace00e3f372b1fd53ea2b0d638c3
maor.shliefer <maor.shliefer@dspg.com>
2023-01-09 01:03:11 +0200

    extract location - wip

--- filter.py
+++ filter.py
@@ -10,7 +10,8 @@ import numpy as np
 def normalize_str(s):
     return s.replace(".", "").replace(",", "").\
             replace("\"", "").\
-            replace("'", "").replace("׳","").strip()
+            replace("'", "").replace("׳","").replace(":","").\
+            replace(")", "").replace("(", "").strip()
 
 
 class LocExtractor:
@@ -24,11 +25,6 @@ class LocExtractor:
                                  for c in city_df['street_name']])
         self.street_names -= {"גבעתיים", "רמת גן"}
 
-        con_words = ["ל", "ב"]
-        self.street_names.update(
-            [cw+c for cw in con_words for c in self.street_names]
-        )
-
     def __call__(self, df_row):
         post_text = str(df_row['text'])
 
@@ -38,16 +34,40 @@ class LocExtractor:
         # TODO: 2-3 words matches
         matches = set()
         for s_name in self.street_names:
-            s_name_split = s_name.split()
-            s_name_w0 = s_name_split[0]
-            s_name_w_indices = np.argwhere(s_name_w0 == post_words)
-            for s_name_w_idx in s_name_w_indices:
-                is_match = all([
-                    (i + s_name_w_idx < len(post_words)) and (
-                                s_name_wi == post_words[i + s_name_w_idx])
-                    for i, s_name_wi in enumerate(s_name_split)])
-                if is_match:
-                    matches.add(s_name)
+            for con_w in ["","ל","ב"]:
+                s_name_con = con_w + s_name
+                s_name_split = s_name_con.split()
+                s_name_w0 = s_name_split[0]
+                s_name_w_indices = np.argwhere(s_name_w0 == post_words)
+                for s_name_w_idx in s_name_w_indices:
+                    is_match = all([
+                        (i + s_name_w_idx < len(post_words)) and (
+                                    s_name_wi == post_words[i + s_name_w_idx])
+                        for i, s_name_wi in enumerate(s_name_split)])
+                    if is_match:
+                        s = set(["רחוב", "ברחוב", "לרחוב", "רח", "ברח", "לרח", "כתובת", "הכתובת", "בכתובת", "מיקום", "המיקום"])
+                        n = set(["בשכונת", "לשכונת"])
+
+                        prev_w = post_words[s_name_w_idx-1][0] \
+                            if s_name_w_idx >= 1 else None
+                        next_w = post_words[s_name_w_idx+len(s_name_split)][0] \
+                            if s_name_w_idx + len(s_name_split) < len(post_words) \
+                            else None
+
+                        if next_w and next_w.isnumeric() and len(next_w) <= 3:
+                            a = "רחוב!"
+                        elif prev_w and prev_w in s:
+                            a = "רחוב?"
+                        elif prev_w and prev_w in n:
+                            a = "שכונה"
+                        else:
+                            a = None
+
+                        if s_name == "דוד" and next_w and\
+                                ("שמש" in next_w or "חשמל" in next_w):
+                            continue
+
+                        matches.add((a, s_name))
         print("----")
         print("Text: %s" % post_text)
         if len(matches) > 0:
