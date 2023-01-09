c1f95695697c405eac1be0183390bb3f86ed0d45
maor.shliefer <maor.shliefer@dspg.com>
2023-01-09 02:51:52 +0200

    basic neighberhood logic for Givataym.

--- filter.py
+++ filter.py
@@ -15,15 +15,25 @@ def normalize_str(s):
 
 
 class LocExtractor:
-    def __init__(self, loc_df):
+    def __init__(self, loc_df, neighber_df):
         self.loc_df = loc_df
 
         # Ramat Gan / Givataym
         city_df = loc_df[loc_df['city_code'].isin([6300, 8600])]
+        self.city_names = set([normalize_str(c) for c in city_df['city_name']])
 
-        self.street_names = set([normalize_str(c)
-                                 for c in city_df['street_name']])
-        self.street_names -= {"גבעתיים", "רמת גן"}
+        self.street_names = set([(normalize_str(city), normalize_str(street))
+                                 for city, street in
+                                 zip(city_df['city_name'],
+                                     city_df['street_name'])
+                                 if normalize_str(street) not in
+                                 self.city_names])
+
+        self.neighberhood_by_street = {}
+        for __, row in neighber_df.iterrows():
+            self.neighberhood_by_street['גבעתיים',
+                normalize_str(row['street'])] = \
+                normalize_str(row['neighberhood'])
 
     def __call__(self, df_row):
         post_text = str(df_row['text'])
@@ -33,8 +43,9 @@ class LocExtractor:
 
         # TODO: 2-3 words matches
         matches = set()
-        for s_name in self.street_names:
-            for con_w in ["","ל","ב"]:
+        con_words = ["","ל","ב"]
+        for c_name, s_name in self.street_names:
+            for con_w in con_words:
                 s_name_con = con_w + s_name
                 s_name_split = s_name_con.split()
                 s_name_w0 = s_name_split[0]
@@ -67,11 +78,13 @@ class LocExtractor:
                                 ("שמש" in next_w or "חשמל" in next_w):
                             continue
 
-                        matches.add((a, s_name))
-        matches0 = [("רחוב", m[1]) for m in matches if m[0] == "רחוב!"]
-        matches1 = [("רחוב", m[1]) for m in matches if m[0] == "רחוב?"]
-        matches2 = [("רחוב", m[1]) for m in matches if m[0] == None]
-        matches3 = [("שכונה", m[1]) for m in matches if m[0] == "שכונה"]
+                        matches.add((a, s_name, c_name,
+                                     self.neighberhood_by_street.get(
+                                         (c_name, s_name))))
+        matches0 = [("רחוב", m[1], m[2], m[3]) for m in matches if m[0] == "רחוב!"]
+        matches1 = [("רחוב", m[1], m[2], m[3]) for m in matches if m[0] == "רחוב?"]
+        matches2 = [("רחוב", m[1], m[2], m[3]) for m in matches if m[0] == None]
+        matches3 = [("שכונה", m[1], m[2], m[1]) for m in matches if m[0] == "שכונה"]
         if len(matches0) > 0:
             filtered_matches = matches0
         elif len(matches1) > 0:
@@ -82,6 +95,21 @@ class LocExtractor:
             filtered_matches = matches3
         else:
             filtered_matches = []
+
+        # filter by cities
+        found_cities = set()
+        for c in self.city_names:
+            for con_w in con_words:
+                city_con = con_w + c
+                if city_con in post_text:
+                    found_cities.add(c)
+
+        street_cities = set([m[2] for m in filtered_matches])
+        final_cities = found_cities & street_cities
+        if len(final_cities) > 0 and len(street_cities) > 0:
+            filtered_matches = [m for m in filtered_matches if m[2] in
+                                found_cities]
+
         print("----")
         print("Text: %s" % post_text)
         if len(matches) > 0:
@@ -105,8 +133,10 @@ def find_latest_csv(csv_glob_inp: str):
 if __name__ == "__main__":
     loc_df = pd.read_csv(find_latest_csv("%s/Streets_*.csv" % DATA_DIR))
     posts_df = pd.read_csv(find_latest_csv("%s/Posts_*.csv" % DATA_DIR))
+    neighber_df = pd.read_csv("%s/Giva_Neighberhoods.csv" % DATA_DIR)
 
-    posts_df['location'] = posts_df.apply(LocExtractor(loc_df), axis=1)
+    posts_df['location'] = posts_df.apply(LocExtractor(loc_df,
+                                                       neighber_df), axis=1)
 
     print(loc_df.head())
     print(posts_df.head())
