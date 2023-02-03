7bc2166e9cd0644b28969038c8e8d565418e48b8
maor.shliefer <maor.shliefer@dspg.com>
2023-02-03 17:52:27 +0200

    job to scrape - wip

--- app.py
+++ app.py
@@ -1,13 +1,44 @@
 import logging
 import threading
 from threading import Thread
+from typing import Set, Dict, Tuple
 
+from flask.ctx import AppContext
 from flask_apscheduler import APScheduler
+from sqlalchemy import BigInteger
 
 from app_factory import create_app
 from config import RECREATE_DB
-from db.models import db
-from flask_jobs import facebook_scrape_1_iteration, thread_manage_jobs
+from db.models import db, FBGroup
+from flask_jobs import thread_manage_jobs
+
+
+def add_facebook_groups(app_ctx: AppContext):
+    # RamatGan / Givataym (public groups)
+    groups = [
+        FBGroup(group_id=1424244737803677,
+                is_private=False, _city_codes='6300|8600',
+                should_scrape=True),
+        FBGroup(group_id=186810449287215,
+                is_private=False, _city_codes='6300|8600',
+                should_scrape=True)
+    ]
+
+    with app_ctx:
+        group_ids = set([g.group_id for g in groups])
+
+        existing_groups = db.session.query(FBGroup).filter(
+            FBGroup.group_id.in_(group_ids)).all()
+        existing_group_ids = set([g.group_id for g in existing_groups])
+
+        for group in groups:
+            if group.group_id in existing_group_ids:
+                continue
+
+            db.session.add(group)
+
+        db.session.commit()
+
 
 if __name__ == "__main__":
 
@@ -19,6 +50,8 @@ if __name__ == "__main__":
             db.drop_all()
         logging.info("Updating\\Creating local DB schema")
         db.create_all()
+
+    add_facebook_groups(app.app_context())
 
     stop_event = threading.Event()
     scheduler = APScheduler(app=app)
--- flask_jobs.py
+++ flask_jobs.py
@@ -1,6 +1,6 @@
 import logging
 import threading
-from datetime import datetime
+import datetime
 
 import numpy as np
 from flask.ctx import AppContext
@@ -14,7 +14,8 @@ def thread_manage_jobs(scheduler: APScheduler, stop_event: threading.Event,
                        app_ctx: AppContext):
     scheduler.add_job(id='Facebook_scrape', func=facebook_scrape_1_iteration,
                       args=(app_ctx,),
-                      trigger="interval", next_run_time=datetime.now(),
+                      trigger="interval",
+                      next_run_time=datetime.datetime.now(),
                       hours=1)
     scheduler.start()
 
@@ -29,19 +30,23 @@ def facebook_scrape_1_iteration(app_ctx: AppContext):
 
     with app_ctx:
         fb_groups = db.session.query(FBGroup).filter(
-            FBGroup.should_scrape is True).all()
+            FBGroup.should_scrape == True).all()
 
         for fb_group in fb_groups:
+            max_days_back = 4 if fb_group.last_scrape_date is None else \
+                ((datetime.datetime.now() -
+                 fb_group.fb_group.last_scrape_date).days + 3)
+
             results_df = scrape.scrape_facebook_posts(
                 fb_group.group_id,
                 max_past_limit=2,
-                max_days_back=4,
-                max_posts=3
+                max_days_back=max_days_back,
+                max_posts=10
             )
 
             existing_posts = db.session.query(FBPost).filter(
-                FBPost.post_id in set(results_df['post_id'])
-            )
+                FBPost.post_id.in_(set(results_df['post_id']))).all()
+
             existing_post_ids = set([p.post_id for p in existing_posts])
 
             for __, post in results_df.iterrows():
